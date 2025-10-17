38e8e36ab8a93bbbc6ee84aacbc8e32dbe609c02
Aleira Sanchez <ans222@cornell.edu>
2025-10-17 18:31:56 -0400

    Refactor buildme.R for new package name
    Updated package references from 'demotool' to 'teamATE_solarPanel_tool'.

--- buildme.R
+++ buildme.R
@@ -3,25 +3,27 @@
 # A script to build your package
 
 # set working directory to package root
-setwd("/cloud/project/demos/rpackage")
+setwd("/cloud/project")
 
 # Unload your package and uninstall it first.
-unloadNamespace("demotool"); remove.packages("demotool")
+unloadNamespace("teamATE_solarPanel_tool"); remove.packages("teamATE_solarPanel_tool")
+
 # Auto-document your package, turning roxygen comments into manuals in the `/man` folder
 devtools::document(".")
 # Load your package temporarily!
 devtools::load_all(".")
 
 # Test out our functions
-demotool::plus_one(x = 1)
+teamATE_solarPanel_tool::plus_one(x = 1) #CHANGE THIS--------
 
 # When finished, remember to unload the package
-unloadNamespace("demotool")
+unloadNamespace("teamATE_solarPanel_tool")
 
 # Then, when ready, document, unload, build, and install the package!
 # For speedy build, use binary = FALSE and vignettes = FALSE
 devtools::document("."); # document the package
-unloadNamespace("demotool"); # unload the package
+unloadNamespace("teamATE_solarPanel_tool"); # unload the package
+
 # Build the package
 devtools::build(pkg = ".", path = getwd(), binary = FALSE, vignettes = FALSE)
 
@@ -33,15 +35,15 @@ rstudioapi::restartSession()
 # such as 
 # install.packages("nameofyourpackagefile.tar.gz", type = "source")
 # or in our case:
-install.packages("demotool_1.0.tar.gz", type = "source")
+install.packages("demotool_1.0.tar.gz", type = "source") #CHANGE THIS--------
 
 
 # Load your package!
-library("demotool")
+library("teamATE_solarPanel_tool")
 
 
 # When finished, remember to unload the package
-unloadNamespace("demotool"); remove.packages("demotool")
+unloadNamespace("teamATE_solarPanel_tool"); remove.packages("teamATE_solarPanel_tool")
 
 # Always a good idea to clear your environment and cache
 rm(list = ls()); gc()
